fdfbb867dbd6f5661b05c06db78eb591c3de1c82
hp <hispgg@gmail.com>
2012-08-18 18:50:43 +0300

    1st

---
+++ linq_test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace linq_test
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{  
+			var db = new northwind_sampleEntities();
+			//////////
+			var fuu=
+			db.Products.Distinct();
+			Console.WriteLine(fuu);
+			var selected = from c in db.Orders select c;
+
+			foreach (var order in selected)
+			{
+				Console.WriteLine(order.OrderID+" " + order.ShipCountry);
+			}
+		}
+	}
+}
---
+++ linq_test/someclass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace linq_test
+{
+	class someclass
+	{
+		private int i = 1;
+		private int o = 1;
+	}
+}
